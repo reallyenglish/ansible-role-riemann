4568b90afee30356dfc4587bb6468aeb60677b14
mitsururike <mitsururike2@gmail.com>
2017-12-25 15:32:22 +0900

    [bugfix] fix third party repo, bump rubocop and QA (#1)
    * [bugfix] fix thirdparty repo, bump rubocop and QA
    
    * fix the failed previous commit
    
    * [bugfix] fix provisioner and directory hierarchy
    
    * [bugfix] fix travisci
    
    * QA
    
    * [bugfix] patch rc script; "-t" was introduced in FreeBSD 11
    
    * [bugfix] fix config to pass the specs
    
    * [bugfix] retry to wait for the ports are available

---
+++ tests/serverspec/spec_helper.rb
@@ -0,0 +1,14 @@
+require "serverspec"
+require "rspec/retry"
+
+set :backend, :ssh
+
+options = Net::SSH::Config.for(host)
+options[:host_name] = ENV["KITCHEN_HOSTNAME"]
+options[:user]      = ENV["KITCHEN_USERNAME"]
+options[:port]      = ENV["KITCHEN_PORT"]
+options[:keys]      = ENV["KITCHEN_SSH_KEY"]
+
+set :host,        options[:host_name]
+set :ssh_options, options
+set :env, LANG: "C", LC_ALL: "C"
